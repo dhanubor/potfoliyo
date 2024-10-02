1f7655f0a67508aee2cf142b192ee553b90993f1
dhanubor <nikhilmistry121203@gmail.com>
2024-10-02 17:42:53 +0600

    this my side

---
+++ app.js
@@ -0,0 +1,9 @@
+var options = {
+    strings: ["Frontend Developer", "YouTuber", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+};
+
+var typed = new Typed(".text", options);
